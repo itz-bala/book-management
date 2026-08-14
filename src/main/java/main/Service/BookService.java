e95760cdc7f961490aac60a07408bbaa006af630
bala <bala43957@gmail.com>
2026-08-14 08:17:33 +0530

    3rd commit

--- src/main/java/main/Service/BookService.java
+++ src/main/java/main/Service/BookService.java
@@ -3,8 +3,16 @@ package main.Service;
 import main.DTO.BookRequestDto;
 import main.DTO.BookResponseDto;
 
+import java.util.List;
+
 public interface BookService {
     BookResponseDto create(BookRequestDto dto);
 
     BookResponseDto getbyid(Long id);
+
+    List<BookResponseDto> getAll();
+
+    void delete(Long id);
+
+    BookResponseDto update(Long id, BookRequestDto dto);
 }
